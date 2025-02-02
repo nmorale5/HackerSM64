c33e4c8836e2869a7de9aa2b7fcc0322ae5e82a4
Adam <halaffamagician@gmail.com>
2025-02-02 13:22:59 -0600

    Test change to test SSH

--- include/config/config_objects.h
+++ include/config/config_objects.h
@@ -110,7 +110,7 @@
 /**
  * The number of chain balls the Chain Chomp has. Vanilla is 5.
  */
-#define CHAIN_CHOMP_NUM_SEGMENTS 5
+#define CHAIN_CHOMP_NUM_SEGMENTS 4
 
 /***************
  * -- POKEY --
